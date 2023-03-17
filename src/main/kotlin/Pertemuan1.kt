e801f231a5514a871c3a32d1cc1ae7488e1ecac9
Ariflutfi <arif.kurniawan@students.amikom.ac.id>
2023-03-17 14:26:45 +0700

    latihan data 1a

---
+++ src/main/kotlin/Pertemuan1.kt
@@ -0,0 +1,25 @@
+// pertemuan 1
+fun main() {
+//    println("hello word!")
+//    println("imaduddin")
+//
+//    print("pekerjaan : ")
+//    val pekerjaan = readLine()
+//
+//    print("alamat : ")
+//    val alamat = readLine()
+//
+//    println("pekerjaan kamu adalah $pekerjaan dan alamat kamu di $alamat")
+
+
+//pertemuan 2
+
+    var namaLengkap: String = "budianto"
+    var alamat: String
+    alamat = "yogyakarta"
+    var umur = 22
+
+    println(namaLengkap)
+    println(alamat)
+    println(umur)
+}
